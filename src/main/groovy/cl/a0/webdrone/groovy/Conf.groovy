c021dd278e35950cc049fd5405245312d747dd3a
Aldrin Martoq <aldrin@martoq.cl>
2015-09-09 17:13:45 -0300

    Conf support.

---
+++ src/main/groovy/cl/a0/webdrone/groovy/Conf.groovy
@@ -0,0 +1,18 @@
+package cl.a0.webdrone.groovy
+
+import java.util.concurrent.TimeUnit
+
+class Conf {
+  protected Browser a0
+  def timeout
+
+  Conf(Browser a0) {
+    this.a0 = a0
+  }
+
+  private timeout = 0
+  def setTimeout(val) {
+    this.timeout = val
+    a0.driver.manage().timeouts().implicitlyWait(val * 1000 as Long, TimeUnit.MILLISECONDS)
+  }
+}
